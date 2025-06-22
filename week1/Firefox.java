f45284a7a1ba8edaf00309d0eec3fa6739b638a7
Gayathri <mygayathri@gmail.com>
2025-06-22 14:31:49 +0530

    Add files via upload
    Week1Assessment

---
+++ week1/Firefox.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browerVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char borrowLogo='f';
+		System.out.println("browserName="+browserName+
+				"\nbrowerVersion="+browerVersion+
+				"\nisVisible="+isVisible+
+				"\nreleaseYear="+releaseYear+
+				"\nborrowLogo="+borrowLogo);
+		
+		
+	}
+
+}
